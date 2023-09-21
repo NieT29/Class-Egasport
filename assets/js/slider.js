866f441f5c3165d762283e5c5350af56bc080376
NieT29 <tienlevan3010@gmail.com>
2023-09-21 18:38:22 +0700

    update2

--- assets/js/slider.js
+++ assets/js/slider.js
@@ -1,3 +1,21 @@
 $(document).ready(function(){
-    $(".owl-carousel").owlCarousel();
+  $('.owl-carousel').owlCarousel({
+    loop:true, // vong lap slider
+    margin:10, // khonag cach giua cac phan tu
+    nav:false, // thanh dieu huong 
+    dots:false, // nut dieu huong
+    autoplay:true, //tu dong chuyen slider
+    autoplayTimeout:5000, //ms
+    responsive:{
+        0:{
+            items:1
+        },
+        600:{
+            items:1
+        },
+        1000:{
+            items:1
+        }
+    }
+})
   });
